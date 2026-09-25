6425d195bc707447b88696df82fc52dfa039dce4
clbuerger-ctrl <cl.buerger@googlemail.com>
2026-09-25 20:33:54 +0200

    YouTube-Titelbilder Katalog #1–#33

---
+++ thumbs.js
@@ -0,0 +1,43 @@
+window.TV_THUMBS={
+  1:{yt:"Mep0y-0QzyQ"},
+  2:{yt:"DhEp6rniceQ"},
+  3:{yt:"BJthdW_HwdM"},
+  4:{yt:"MLvRy9MnyQs"},
+  5:{yt:"NeENa66jE-8"},
+  6:{yt:"yyDBPGPifLc"},
+  7:{yt:"LKu0ESeXLIQ"},
+  8:{yt:"YVRVKL7nNCM"},
+  9:{yt:"Tev6XEV62sg"},
+  10:{yt:"8fRQgFNJBHs"},
+  11:{yt:"sMh38kdc1Mw"},
+  12:{yt:"5ZGiTdqHWVw"},
+  13:{yt:"PcEMtR8lWK0"},
+  14:{yt:"fp8A8kUU554"},
+  15:{yt:"aFFLH2EByMQ"},
+  16:{yt:"ZgcjZyYdu8A"},
+  17:{yt:"FHQAcpyAlIw"},
+  18:{yt:"ZVkyLQkYB4U"},
+  19:{yt:"-peT0prqSaE"},
+  20:{yt:"sZKH90OrqR4"},
+  21:{yt:"86_uzU3CBa4"},
+  22:{yt:"Fp-R6_mxzl8"},
+  23:{yt:"9cu8ySJN2mg"},
+  24:{yt:"kNhX1jTMUxg"},
+  25:{yt:"wQcYq6r-VbQ"},
+  26:{yt:"zyprzXTeTRY"},
+  27:{yt:"ESpdsVYbK_0"},
+  28:{yt:"NMx5lYer-CY"},
+  29:{yt:"WaL8PFTdKhM"},
+  30:{yt:"otCTcblX4ZQ"},
+  31:{yt:"F4sG8NK0eCo"},
+  32:{yt:"BDb-dPKlQfE"},
+  33:{yt:"GS2DYnsAcbY"}
+};
+window.TV_THUMBS_FILE={
+  "EP28":{yt:"DbsMnaqQzP0"},
+  "EP29":{yt:"p3FPW368U0M"}
+};
+function tvThumbUrl(yt){
+  if(!yt) return "cover.jpg";
+  return "https://i.ytimg.com/vi/"+yt+"/hqdefault.jpg";
+}
